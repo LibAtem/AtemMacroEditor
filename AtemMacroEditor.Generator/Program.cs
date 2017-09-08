b0e9981208d5c96f2b78750ca69a61f60c03fee0
Julian <git@julusian.co.uk>
2017-09-09 00:30:13 +0100

    xml spec file generation

---
+++ AtemMacroEditor.Generator/Program.cs
@@ -0,0 +1,146 @@
+﻿using LibAtem.MacroOperations;
+using LibAtem.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Serialization;
+
+namespace AtemMacroEditor.Generator
+{
+    public class Program
+    {
+        public static void SaveState(string path, XmlSpec profile)
+        {
+            var ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(XmlSpec));
+            FileStream fs = new FileStream(path, FileMode.Create);
+            serializer.Serialize(fs, profile, ns);
+            fs.Flush();
+            fs.Dispose();
+        }
+
+        private static XmlSpec CompileData()
+        {
+            var res = new XmlSpec();
+
+            foreach (var op in MacroOpManager.FindAll())
+            {
+                var xmlOp = new XmlOperation() {Id = op.Key.ToString()};
+                res.Operations.Add(xmlOp);
+                
+                IEnumerable<PropertyInfo> props = op.Value.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).Where(prop => prop.GetCustomAttribute<NoSerializeAttribute>() == null);
+                foreach (PropertyInfo prop in props)
+                {
+                    var fieldAttr = prop.GetCustomAttribute<MacroFieldAttribute>();
+                    if (fieldAttr == null)
+                        continue;
+
+                    var xmlField = new XmlField()
+                    {
+                        Id = fieldAttr.Id,
+                        Name = fieldAttr.Name,
+                    };
+                    xmlOp.Fields.Add(xmlField);
+
+                    if (prop.GetCustomAttribute<BoolAttribute>() != null)
+                    {
+                        xmlField.Type = FieldType.Bool;
+                    }
+                    else if (prop.GetCustomAttribute<Enum8Attribute>() != null || prop.GetCustomAttribute<Enum16Attribute>() != null || prop.GetCustomAttribute<Enum32Attribute>() != null)
+                    {
+                        xmlField.Type = FieldType.Enum;
+                        var vals = Enum.GetValues(prop.PropertyType);
+
+                        foreach (object val in vals)
+                        {
+                            // TODO check value is available for device profile/usage location
+                            xmlField.Values.Add(new XmlFieldValue()
+                            {
+                                Id = ((int)val).ToString(),
+                                Name = val.ToString(),
+                            });
+                        }
+                    }
+                    else
+                    {
+                        SetNumericProps(xmlField, prop);
+                    }
+                }
+
+            }
+
+            return res;
+        }
+
+        private static void SetNumericProps(XmlField field, PropertyInfo prop)
+        {
+            var uint16range = prop.GetCustomAttribute<UInt16RangeAttribute>();
+            if (uint16range != null)
+            {
+                field.Type = FieldType.Int;
+                field.Min = uint16range.Min;
+                field.Max = uint16range.Max;
+                return;
+            }
+            var uint8range = prop.GetCustomAttribute<UInt8RangeAttribute>();
+            if (uint8range != null)
+            {
+                field.Type = FieldType.Int;
+                field.Min = uint8range.Min;
+                field.Max = uint8range.Max;
+                return;
+            }
+
+            var int16d = prop.GetCustomAttribute<Int16DAttribute>();
+            if (int16d != null)
+            {
+                field.Type = FieldType.Double;
+                field.Min = int16d.ScaledMin;
+                field.Max = int16d.ScaledMax;
+                field.Scale = int16d.Scale;
+                return;
+            }
+            var int32d = prop.GetCustomAttribute<Int32DAttribute>();
+            if (int32d != null)
+            {
+                field.Type = FieldType.Double;
+                field.Min = int32d.ScaledMin;
+                field.Max = int32d.ScaledMax;
+                field.Scale = int32d.Scale;
+                return;
+            }
+
+            var uint16 = prop.GetCustomAttribute<UInt16Attribute>();
+            if (uint16 != null)
+            {
+                field.Type = FieldType.Int;
+                field.Min = 0;
+                field.Max = (int)Math.Pow(2, 16) - 1;
+                return;
+            }
+            var uint8 = prop.GetCustomAttribute<UInt8Attribute>();
+            if (uint8 != null)
+            {
+                field.Type = FieldType.Int;
+                field.Min = 0;
+                field.Max = (int)Math.Pow(2, 8) - 1;
+                return;
+            }
+
+            throw new Exception(string.Format("Unknown field type: {0}.{1}", field.Name, prop.Name));
+        }
+
+        static void Main(string[] args)
+        {
+            var spec = CompileData();
+
+            SaveState("../spec.xml", spec);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AtemMacroEditor.Generator/XmlSpec.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace AtemMacroEditor.Generator
+{
+    [XmlRoot("MacroOperations", IsNullable = false)]
+    public class XmlSpec
+    {
+        public XmlSpec()
+        {
+            Operations = new List<XmlOperation>();
+        }
+
+        [XmlArrayItem("Op")]
+        public List<XmlOperation> Operations { get; set; }
+    }
+
+    public class XmlOperation
+    {
+        public XmlOperation()
+        {
+            Fields = new List<XmlField>();
+        }
+
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlElement("Field")]
+        public List<XmlField> Fields { get; set; }
+    }
+
+    public enum FieldType
+    {
+        Enum,
+        Int,
+        Double,
+        Bool,
+    }
+
+    public class XmlField
+    {
+        public XmlField()
+        {
+            Values = new List<XmlFieldValue>();
+        }
+
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("type")]
+        public FieldType Type { get; set; }
+
+        [XmlAttribute("min")]
+        public int Min { get; set; }
+        public bool ShouldSerializeMin()
+        {
+            return Type == FieldType.Int || Type == FieldType.Double;
+        }
+
+        [XmlAttribute("max")]
+        public int Max { get; set; }
+        public bool ShouldSerializeMax()
+        {
+            return Type == FieldType.Int || Type == FieldType.Double;
+        }
+
+        [XmlAttribute("scale")]
+        public double Scale { get; set; }
+        public bool ShouldSerializeScale()
+        {
+            return Type == FieldType.Double;
+        }
+
+        [XmlElement("Value")]
+        public List<XmlFieldValue> Values { get; set; }
+    }
+
+    public class XmlFieldValue
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+    }
+}
